f60be2191ee6fe36b386e4291c7193be7f66dfec
Takuya Otsuka <tak.otsuka@datacurrent.co.jp>
2025-05-27 12:34:14 +0900

    initial commit

---
+++ sample.js
@@ -0,0 +1,56 @@
+let mediaRecorder;
+const availableSize = document.getElementById("available-size");
+const MEDIA_RECORDER_TIME_SLICE = 1000; // 1 second
+
+document.getElementById("prepareButton").addEventListener("click", async function() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        let bufferedChunks = [];
+        mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.ondataavailable = function(event) {
+            console.log("Data available:", event.data);
+            availableSize.textContent = event.data.size;
+
+            if (event.data.size > 0) {
+                bufferedChunks.push(event.data);
+            }
+        };
+        mediaRecorder.onstop = function() {
+            console.log("Recording stopped");
+            const audioBlob = new Blob(bufferedChunks);
+            const audioUrl = URL.createObjectURL(audioBlob);
+            const audioElement = document.getElementById("recordedAudio");
+            audioElement.src = audioUrl;
+        };
+        console.log("Recording started");
+    } catch (error) {
+        console.error("Error accessing media devices.", error);
+    }
+});
+
+document.getElementById("playStartButton").addEventListener("click", function() {
+    const audioElement = document.getElementById("sampleAudio");
+    audioElement.onended = function() {
+        mediaRecorder?.start(MEDIA_RECORDER_TIME_SLICE);
+    };
+    audioElement.play().catch(error => {
+        console.error("Error playing audio:", error);
+    });
+});
+
+document.getElementById("playButton").addEventListener("click", function() {
+        const audioElement = document.getElementById("sampleAudio");
+    audioElement.onended = function() {
+    };
+    audioElement.play().catch(error => {
+        console.error("Error playing audio:", error);
+    });
+});
+
+document.getElementById("startButton").addEventListener("click", function() {
+    mediaRecorder?.start(MEDIA_RECORDER_TIME_SLICE);
+});
+
+document.getElementById("stopButton").addEventListener("click", function() {
+    mediaRecorder?.stop();
+});
